481f7234a7b36e562b8ee7cc5340fe57b1d857de
Victor Hugo <projektvek@gmail.com>
2025-05-21 14:10:28 -0300

    :sparkles: model da categoria e produtos criados

---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,69 @@
+package com.generation.lojagames.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100)
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Size(min = 2, max = 100, message = "O atributo nome deve ter no mínimo 2 e no máximo 100 caracteres")
+	private String nome;
+	
+	@Column(precision = 7, scale = 2)
+	@NotBlank(message = "O atributo preco é obrigatório")
+	private BigDecimal preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
